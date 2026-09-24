a48933fb6014b4607d2bbe7738421c70e31f12b5
Sarocha1130 <67050556@kmitl.ac.th>
2026-09-24 10:11:29 +0700

    cell05 ex01 play_with_arrays.py

---
+++ cell05/ex01/play_with_arrays.py
@@ -0,0 +1,8 @@
+numbers = [2, 8, 9, 48, 8, 22, -12, 2]
+new_numbers = []
+
+for number in numbers:
+    new_numbers.append(number + 2)
+
+print(f"Original array: {numbers}")
+print(f"New array: {new_numbers}")
